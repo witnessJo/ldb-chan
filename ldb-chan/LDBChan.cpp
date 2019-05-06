15306cae67aabb79b775d350d52fbafa28655b6d
jongyoungcha <jongyoungcha@gmail.com>
2019-05-06 03:19:16 -0400

    Add Submodules and CMakesLists.txt files.

---
+++ ldb-chan/LDBChan.cpp
@@ -0,0 +1,7 @@
+#include "common.h"
+
+using namespace std;
+
+int main(){
+   return 0;
+}
---
+++ ldb-chan/common.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include <thread>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
